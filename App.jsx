37f938b8c6cfeaeb38eb4432e19f3bc49f23f86d
rosembergb-debug <rosembergbruna@gmail.com>
2025-08-05 23:07:40 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FreteCalculator from "./FreteCalculator";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Agente de Cotações de Frete</h1>
+      <FreteCalculator />
+    </main>
+  );
+}
+
+export default App;
---
+++ FreteCalculator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FreteCalculator() {
+  return (
+    <div>
+      <p>Formulário de cotação aqui (em desenvolvimento)...</p>
+    </div>
+  );
+}
+
+export default FreteCalculator;
